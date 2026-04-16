092762d19b71a4c3c3fca5707c3a5bd427e4c970
Luis Quan <primoquan@gmail.com>
2026-04-16 14:53:38 -0600

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+//Redireccionamos nuestra web al login
 
-export const routes: Routes = [];
+  import { Routes } from '@angular/router';
+  import { LoginComponent } from './pages/login/login.component';
+  import { PerfilComponent } from './pages/perfil/perfil.component';
+
+  export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'perfil', component: PerfilComponent },
+  ];
