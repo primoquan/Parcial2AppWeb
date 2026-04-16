be426d903c65db865f84da4d1364e0d2f67eedda
Luis Quan <primoquan@gmail.com>
2026-04-16 15:11:47 -0600

    Update seed.sql
    nuevos hash creado

--- database/seed.sql
+++ database/seed.sql
@@ -2,15 +2,15 @@
 --
 
   INSERT INTO usuarios (username, correo, password) VALUES
-    ('admin',     'admin@test.com',     '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario1',  'usuario1@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario2',  'usuario2@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario3',  'usuario3@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario4',  'usuario4@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario5',  'usuario5@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario6',  'usuario6@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario7',  'usuario7@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario8',  'usuario8@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario9',  'usuario9@test.com',  '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'),
-    ('usuario10', 'usuario10@test.com', '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy')
+    ('admin',     'admin@test.com',     '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario1',  'usuario1@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario2',  'usuario2@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario3',  'usuario3@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario4',  'usuario4@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario5',  'usuario5@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario6',  'usuario6@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario7',  'usuario7@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario8',  'usuario8@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario9',  'usuario9@test.com',  '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.),
+    ('usuario10', 'usuario10@test.com', '$2a$10$1A9aMFNmIZNMmPRef1tHM.VepryHaD7BG.SH5joxmt5WgFFnWGRa.)
   ON CONFLICT (username) DO NOTHING;
