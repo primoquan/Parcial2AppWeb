74186dce11faf84feb4bbe6d5adda15dd7fe40bd
Luis Quan <primoquan@gmail.com>
2026-04-16 14:55:04 -0600

    Update app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+  import { Component } from '@angular/core';
+  import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('frontend');
-}
+  @Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    template: '<router-outlet />',
+  })
+  export class App {}
