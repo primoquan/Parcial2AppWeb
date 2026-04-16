a33ddbaf33baa34e6bbe03604dad81f08064e94d
Luis Quan <primoquan@gmail.com>
2026-04-16 14:54:31 -0600

    Update app.config.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+//Agregamos para que pueda hacer las peticiones al backend
 
-import { routes } from './app.routes';
+  import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+  import { provideRouter } from '@angular/router';
+  import { provideHttpClient } from '@angular/common/http';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
-};
+  import { routes } from './app.routes';
+
+  export const appConfig: ApplicationConfig = {
+    providers: [
+      provideBrowserGlobalErrorListeners(),
+      provideRouter(routes),
+      provideHttpClient(),
+    ]
+  };
